cebc7977cda6619910b78d9415f217958c62d8aa
Alexandr <ama689113@yandex.ru>
2024-04-09 13:23:55 +0300

    начальный-урок

---
+++ js/script.js
@@ -0,0 +1,63 @@
+"use strict"
+
+// Правильно писать переменные
+// shake_case
+// UPPER_SNAKE_CASE - неизменые переменые, нельзя менять по коду
+// Kebab-case
+// PascalCase
+
+// простые типы данных
+// null когда нет в коде например переменной которую мы пытаемся вывести
+// undefined когда нет значения например есть переменная но нет значения
+
+// масив const arr = [1, 2, 3,];
+// обьект const obj = {a: 1, b: 2};
+
+// typeof() узнать тип данных 
+// обсолютно все данные от пользователя приходят в виде строки
+
+// let nameUser = alert("Введите свое имя: ");
+// let nameUser = confirm("Введите свое имя: ");
+// let ageUser = prompt("Введите ваш возраст:", "18");
+// + оператор плюс преобразовывает str в number
+// let ageUser = +prompt("Введите ваш возраст:", "18");
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+// интерполяция console.log(`Привет ${nameUser}!`);
+// const category = "catalog";
+// console.log(`https://sitename.ru/${category}/5`);
+
+
+ // let incr = 10,
+ //    decr = 10;
+
+// incr++; // инкремент 
+// decr--; // декремент
+// incr++; // Префиксный инкремент
+// ++incr; // Постфиксный инкремент
+
+// console.log(--incr); - Постфиксный декремент
+// console.log(++decr); - Постфиксный инкремент
+// console.log(50%3);  - остаток от деление
+
+// console.log(2+4 == "6"); - знак равенства
+
+// console.log(2+4 === "6"); - знак строгого равенства
+
+// const isChecked = false,
+//       isClose = true;
+
+// console.log(isChecked && isClose); - оператор "&& "И" - все значения должны быть true
+// console.log(isChecked || isClose); - оператор || ИЛИ хоть одно значение true 
+// console.log(isChecked || !isClose); - оператор отрицания значения "!"
+
+// console.log(2 + 2 * 2 != 6); - оператор неравенства "!="
+// console.log(2 + 2 * 2 !== "6"); - оператор неравенства со строгой типизацией
+
